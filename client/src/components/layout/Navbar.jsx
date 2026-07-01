86337faab43e6e01811b216ca32c1844d33ce743
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-07-01 21:46:05 +0530

    feat:Added lucid style icons

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,12 +1,16 @@
-
+import {Wallet} from "lucide-react"
 
 function Navbar(){
 
     return(
 
         <nav className="h-16 bg-white border-b px-6 flex items-center justify-between">
-            <h1 className="text-xl font-bold text-green-600">
+            <h1 className="flex items-center gap-2 text-xl font-bold text-green-600">
+
+                <Wallet className="w-7 h-7"/>
+
                 Expense Tracker
+
             </h1>
 
             <div className="text-xl font-bold text-green-600">
--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -1,31 +1,38 @@
+import { LayoutDashboard , Receipt , IndianRupee , ChartColumn ,User, Wallet  } from "lucide-react";
+
 function Sidebar() {
   return (
     <aside className="w-66 bg-white border-r min-h-screen p-5">
 
-      <h2 className="text-2xl font-bold text-green-600 mb-8">
-        💰 Expense Tracker
+      <h2 className="flex gap-2 items-center text-2xl font-bold text-green-600 mb-8">
+        <Wallet className="h-7 w-7"/>
+        Expense Tracker
       </h2>
 
       <nav className="flex flex-col gap-3">
-
-        <button className="text-left p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
-          🏠 Dashboard
+        <button className="flex items-center gap-3 p-3 rounded-lg hover:bg-green-100 transition-colors duration-300">
+          <LayoutDashboard className="w-5 h-5"/> 
+          Dashboard
         </button>
 
-        <button className="text-left p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
-          💸 Expenses
+        <button className="flex items-center gap-3 p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
+          <Receipt className="w-5 h-5"/>
+          Expenses
         </button>
 
-        <button className="text-left p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
-          💵 Income
+        <button className="flex items-center gap-3 p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
+          <IndianRupee className="w-5 h-5"/>
+          Income
         </button>
 
-        <button className="text-left p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
-          📊 Analytics
+        <button className="flex items-center gap-3 p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
+          <ChartColumn className="w-5 h-5"/>
+          Analytics
         </button>
 
-        <button className="text-left p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
-          👤 Profile
+        <button className="flex items-center gap-3 p-3 rounded-lg hover:bg-green-100 transition-all duration-300">
+          <User className="w-5 h-5"/>
+          Profile
         </button>
 
       </nav>
